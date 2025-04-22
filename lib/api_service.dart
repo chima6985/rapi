936812af0c4c7258cba580ee1e2c29ebf1e6aacb
chima6985 <chimaokeke2006@gmail.com>
2025-04-22 10:37:33 +0100

    getting and using the model

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -1,9 +1,20 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:rapi_app/user_model.dart';
 
 class ApiService {
   final String baseUrl = "https://reqres.in/api";
-Future<void> fetchUser() async{
-  http.get(Uri.parse('$baseUrl'));
+Future<List<User>> fetchUser() async{
+final response = await http.get(Uri.parse('$baseUrl/users'));
+
+if (response.statusCode == 200) {
+ final List<dynamic> data = jsonDecode(response.body) ['data'];
+ return  data.map((item) =>User.fromJson(item)).toList();
+}
+else {
+  throw Exception('Failed to Load the User');
+}
 }
 
 }
