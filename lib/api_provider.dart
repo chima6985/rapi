f9b6b2b1e5f0ae484faf0bd8e3544bb9d0ba0580
chima6985 <chimaokeke2006@gmail.com>
2025-04-22 11:11:23 +0100

    Aded providerscope

--- lib/api_provider.dart
+++ lib/api_provider.dart
@@ -1,10 +1,13 @@
 import 'package:rapi_app/api_service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:rapi_app/user_model.dart';
 
 final apiServiceProvider = Provider<ApiService>((ref) {
   return ApiService();
 });
 
-
+ final userLlistProvider = FutureProvider<List<User>>((ref) async {
+  return ref.watch(apiServiceProvider).fetchUser() ;
+ });
 
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:rapi_app/api_provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -9,16 +11,42 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build( BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurpleAccent),
         useMaterial3: true,
       ),
-      home: Scaffold(
-        appBar: AppBar(title: Center(child: const Text('USER LIST'))),
-      ),
+      home: UserListScreen(),
+    );
+  }
+}
+
+class UserListScreen extends ConsumerWidget {
+  const UserListScreen({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userList = ref.watch(userLlistProvider);
+    return Scaffold(
+      appBar: AppBar(title: Center(child: const Text('USER LIST'))),
+      body: userList.when(
+      data: (users){
+        return ListView.builder(
+          itemCount: users.length,
+          itemBuilder: (context, index) {
+         final user =  users[index];
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(user.avatar),
+            ),
+            title: Text('${user.firstName} ${user.lastName}'),
+            subtitle: Text(user.email),
+          );
+        });
+      },
+      error: (error, stack) => Text(error.toString()),
+      loading: () => Center(child: CircularProgressIndicator())),
     );
   }
 }
