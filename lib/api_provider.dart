e296643b579bb7cf0a490358c0a21caae3486ff9
chima6985 <chimaokeke2006@gmail.com>
2025-04-22 10:50:15 +0100

    creating the API service

---
+++ lib/api_provider.dart
@@ -0,0 +1,10 @@
+import 'package:rapi_app/api_service.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final apiServiceProvider = Provider<ApiService>((ref) {
+  return ApiService();
+});
+
+
+
+
